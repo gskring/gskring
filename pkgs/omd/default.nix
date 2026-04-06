826d9b3ed68cdf220fcfd4f6e8c80404e3f45916
Yifei Sun <ysun@hey.com>
2026-04-06 13:16:25 +0200

    update patched packages for liquid support

--- pkgs/omd/default.nix
+++ pkgs/omd/default.nix
@@ -1,6 +1,5 @@
 { buildDunePackage
 , fetchFromGitHub
-, fetchpatch2
 , dune-build-info
 , ppx_expect
 , uucp
@@ -18,14 +17,6 @@ buildDunePackage (finalAttrs: {
     tag = finalAttrs.version;
     hash = "sha256-5eZitDaNKSkLOsyPf5g5v9wdZZ3iVQGu8Ot4FHZZ3AI=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      name = "0001-omd-footnote-support.patch";
-      url = "https://patch-diff.githubusercontent.com/raw/ocaml-community/omd/pull/309.patch";
-      hash = "sha256-mlzwQCX9QdIRbKkZbC91f7MzuVTpYDatcvdGdV07B4k=";
-    })
-  ];
 
   env.DUNE_CACHE = "disabled";
 
